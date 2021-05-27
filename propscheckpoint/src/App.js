e808d257adbe59d5dc03fb993c1543eb18e04d4b
WRD-MDI <werda.mehdi@gmail.com>
2021-05-27 17:38:50 +0200

    propscheckpoint

--- propscheckpoint/src/App.js
+++ propscheckpoint/src/App.js
@@ -3,9 +3,18 @@ import Profile from "./profile/Profile";
 import { myinformation } from "./profile/myinformation";
 
 function App() {
+  const handelName = (name) => {
+    alert(name);
+  };
   return (
     <div className="App">
-      <Profile information={myinformation} />
+      <Profile information={myinformation} handelName={handelName}>
+        <img
+          src="https://cdn.pixabay.com/photo/2017/06/13/12/53/profile-2398782_960_720.png"
+          alt="pfoto"
+          style={{ width: "15%" }}
+        />
+      </Profile>
     </div>
   );
 }
--- propscheckpoint/src/profile/Profile.js
+++ propscheckpoint/src/profile/Profile.js
@@ -1,14 +1,32 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Profile(props) {
+function Profile({
+  handelName,
+  information: { fullName, profession, bio },
+  children,
+}) {
   return (
     <div>
-      <h1>{props.information.fullName}</h1>
-      <h2>{props.information.profession}</h2>
-      <h2>{props.information.bio}</h2>
-      <img src=
+      {children}
+      <h1>{fullName}</h1>
+      <h2>{profession}</h2>
+      <h2>{bio}</h2>
+      <button onClick={() => handelName(fullName)}>Show name</button>
     </div>
   );
 }
+
+Profile.defaultProps = {
+  handelName: () => {
+    alert("there is no functionw");
+  },
+};
+
+Profile.propTypes = {
+  information: PropTypes.shape({
+    fullName: PropTypes.number.isRequired,
+  }),
+};
 
 export default Profile;
